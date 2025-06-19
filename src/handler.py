151a6f92b5222a0583292223cbca9452b13cbfee
f177 <ffelippee4@gmail.com>
2025-06-19 15:46:57 -0300

    diagon6

--- src/handler.py
+++ src/handler.py
@@ -1,7 +1,9 @@
+import os
 import time
 import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
+import sys
 
 LOCAL_URL = "http://127.0.0.1:3000/sdapi/v1"
 
@@ -10,7 +12,6 @@ retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
 automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
 
 def wait_for_service(url):
-    """Verifica se o serviço está pronto para receber requisições."""
     retries = 0
     while True:
         try:
@@ -20,20 +21,17 @@ def wait_for_service(url):
             retries += 1
             if retries % 15 == 0:
                 print("Serviço ainda não está pronto. Tentando novamente...")
-        except Exception as err:
-            print("Erro ao esperar pelo serviço: ", err)
         time.sleep(0.2)
 
 def run_inference(inference_request):
-    """Executa a inferência."""
     response = automatic_session.post(url=f'{LOCAL_URL}/txt2img', json=inference_request, timeout=600)
     return response.json()
 
 def handler(event):
-    """O handler que será chamado pelo RunPod Serverless."""
-    json_output = run_inference(event["input"])
-    return json_output
-
+    result = run_inference(event["input"])
+    print("Job executado. Finalizando processo.")
+    os._exit(0)  # Encerra imediatamente TODO o processo, acionando o trap no start.sh
+    return result
 
 if __name__ == "__main__":
     wait_for_service(url=f'{LOCAL_URL}/sd-models')
