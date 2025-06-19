15a2121cad972a672ffc8e8875d127ec4410881f
f177 <ffelippee4@gmail.com>
2025-06-19 01:11:41 -0300

    diagon1

--- src/start.sh
+++ src/start.sh
@@ -2,25 +2,29 @@
 
 echo "Worker Initiated"
 
-echo "Starting WebUI API"
+# Start the WebUI API in the background
+echo "Starting Automatic1111 API..."
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 export PYTHONUNBUFFERED=true
-python /stable-diffusion-webui/webui.py \
-  --xformers \
-  --no-half-vae \
-  --skip-python-version-check \
-  --skip-torch-cuda-test \
-  --skip-install \
-  --ckpt /model.safetensors \
-  --opt-sdp-attention \
-  --disable-safe-unpickle \
-  --port 3000 \
-  --api \
-  --nowebui \
-  --skip-version-check \
-  --no-hashing \
-  --no-download-sd-model &
 
-echo "Starting RunPod Handler"
+# Launch the webui.py script with corrected arguments
+python /stable-diffusion-webui/webui.py \
+    --xformers \
+    --no-half-vae \
+    --skip-python-version-check \
+    --skip-torch-cuda-test \
+    --skip-install \
+    --ckpt /model.safetensors \
+    --opt-sdp-attention \
+    --disable-safe-unpickle \
+    --port 3000 \
+    --api \
+    --nowebui \
+    --skip-version-check \
+    --no-hashing \
+    --no-download-sd-model &
+
+# Immediately start the handler (NOT RECOMMENDED)
+echo "Starting RunPod Handler without health check..."
 python -u /handler.py
